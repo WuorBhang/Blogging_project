e73d35824e97ba484394e3de18ef4cb39b99b0d7
Wuor Bhang <143351848+WuorBhang@users.noreply.github.com>
2024-09-30 11:29:16 +0300

    Update settings.py

--- Blogging_Project/settings.py
+++ Blogging_Project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a27yjak@$p8ury&urn7zf_!7y0(ia^sgrje7s8$#33rbrv^$%d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ ]
+ALLOWED_HOSTS = []
 
 
 # Application definition
