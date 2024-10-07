3a2ca6592a6cacdb59b06bc8a14bdfd09689dbdb
Wuor Bhang <uhuribhang211@gmail.com>
2024-10-07 14:24:56 +0300

    adding blog project files

--- blog/models.py
+++ blog/models.py
@@ -44,6 +44,7 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.user.username} on {self.post.title}"
